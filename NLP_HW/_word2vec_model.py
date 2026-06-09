7b9ef4c3c06d4d3c8f9cb96c175405dc5331b2bb
lichang-jin <3517179979@qq.com>
2026-06-09 14:57:41 +0800

    update

--- NLP_HW/_word2vec_model.py
+++ NLP_HW/_word2vec_model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
 import pandas as pd
 import numpy as np
 from gensim.models import KeyedVectors
@@ -8,10 +9,12 @@ from utils import random_word_vector, process_dataset, train, test
 
 
 class Config:
-    def __init__(self, mode = "train"):
+    def __init__(self, mode = "train", net = "rnn"):
         self.tokens2id = pd.read_csv('dataset/tokens2id.csv').set_index('tokens')
         # the param of the network
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.mode = mode
+        self.net = net
         self.seed = 42
         self.lr = 1e-4
         self.weight_decay = 1e-6
@@ -20,12 +23,16 @@ class Config:
         self.num_workers = 4
         self.dropout = 0.5
         self.num_classes = 5
+        # CNN
         self.filter_size = [3, 4, 5]
         self.num_filters = 100
-        self.save_path = "model/word2vec_model.pth"
-        self.mode = mode
+        # RNN
+        self.num_layers = 1
+        self.hidden_size = 128
+        self.direction = 2
+        self.save_path = "model/word2vec_model.pth" if self.net != "rnn" else "model/word2vec_model_rnn.pth"
         # the param of sentence embedding
-        self.sentence_length = 50
+        self.sentence_length = 64
         self.embedding_dim = 300
         self.vocab_size = self.tokens2id.shape[0] + 1
 
@@ -37,8 +44,17 @@ class Model(nn.Module):
         self.embedding = nn.Embedding(self.config.vocab_size, self.config.embedding_dim)
         if config.mode == "train":
             self.word2vec_init()
+        self.lstm = nn.LSTM(
+            input_size = self.config.embedding_dim,
+            hidden_size = self.config.hidden_size,
+            num_layers = self.config.num_layers,
+            dropout = self.config.dropout if self.config.num_layers > 1 else 0,
+            bidirectional = self.config.direction > 1,
+            batch_first = True,
+        )
+        self.kernel_size = self.config.embedding_dim if self.config.net != "rnn" else self.config.hidden_size * self.config.direction
         self.conv = nn.ModuleList([
-            nn.Conv2d(1, self.config.num_filters, (k, self.config.embedding_dim))
+            nn.Conv2d(1, self.config.num_filters, (k, self.kernel_size))
             for k in self.config.filter_size
         ])
         self.fc = nn.Sequential(
@@ -66,14 +82,28 @@ class Model(nn.Module):
         self.embedding.weight.requires_grad = True
 
     def forward(self, x):
-        x = self.embedding(x)                               # B x L x D
-        x = x.unsqueeze(1)                                  # B x 1 x L x D
+        lengths = (x != 0).sum(dim=1).view(-1).cpu().tolist()   # B x L
+        x = self.embedding(x)                                   # B x L x D
+        if self.config.net == "rnn":
+            x_packed = pack_padded_sequence(
+                x,
+                lengths,
+                batch_first=True,
+                enforce_sorted=False,
+            )
+            x_n, (h_n, c_n) = self.lstm(x_packed)               # B x L x H
+            x, _ = pad_packed_sequence(
+                x_n,
+                batch_first=True,
+                total_length=self.config.sentence_length,
+            )
+        x = x.unsqueeze(1)                                      # B x 1 x L x H
         out = []
         for conv in self.conv:
-            h = self.relu(conv(x)).squeeze(-1)              # B x C x L
-            h = F.max_pool1d(h, h.size(-1)).squeeze(-1)     # B x C
+            h = self.relu(conv(x)).squeeze(-1)                  # B x C x L
+            h = F.max_pool1d(h, h.size(-1)).squeeze(-1)         # B x C
             out.append(h)
-        out = torch.cat(out, 1)                        # B x C'
+        out = torch.cat(out, 1)                            # B x C'
         out = self.dropout(out)
         out = self.fc(out)
         return out
